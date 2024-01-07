0d118bc656206a9bcc9e71e9761da6e312f1deac
Vinicius Gabriel <vinihbieel49@gmail.com>
2024-01-06 22:40:54 -0300

    openGraph update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,13 +13,35 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Vinicius Gabriel - Portfolio",
   description: "Vinicius Gabriel fullstack developer portfolio",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://my-portfolio-gattekaa.vercel.app/",
+    siteName: "Vinicius Gabriel - Portfolio",
+    title: "Vinicius Gabriel - Portfolio",
+    description: "Vinicius Gabriel fullstack developer portfolio",
+    images: [
+      {
+        url: "/brand_icon_transparent_green.svg",
+        width: 800,
+        height: 600,
+        alt: "Vinicius Gabriel - Portfolio",
+      },
+    ],
+  },
   icons: [
     {
       rel: "icon",
       url: "/brand_icon_transparent_green.svg",
       sizes: "any",
-    },
+    }
   ],
+  authors: [
+    {
+      name: "Vinicius Gabriel"
+    }
+  ],
+
   robots: "index, follow",
 
 };
